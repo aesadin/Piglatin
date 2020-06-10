00345859dc8c51c7c30fea75d6f88121050abd93
cmbrauch <65939504+cmbrauch@users.noreply.github.com>
2020-06-10 19:04:27 -0400

    add new functions to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -10,10 +10,47 @@ function wayToWord(vowelWord) {
     return (vowelWord + "way");
 }
 
-function plusAy(consonant) {
-    return (consonant + "ay");
+
+function plusAy(consonantWord) {
+    let splitString = consonantWord.split("");
+    let sliceString = splitString.slice(1);
+    let joinString = sliceString.join("");
+    let zeroString = splitString.toString("");
+    return (joinString + zeroString[0] + "ay");
 }
 
+
+function moveConsonants(word) {
+    const vowels = ["a", "e", "i", "o", "u"];
+    let wordArray = word.split("");
+    for(i=0; i <wordArray.length; i++) {
+        let vowelMatch = false
+        for(v=0; v <vowels.length; v++) {
+            if (vowels[v] === wordArray[i]) {
+                return true
+                //vowelMatch = true;
+                //break;
+                //console.log(wordArray)
+            };
+        };
+
+        //christening
+        //const 1 =chr
+        //const 2 istening
+      //const 3 =  const 2 (concat)const 1 "ay"
+      //isteningchray
+    
+};
+};
+//vowel = ["a", "e", "i", "o", "u"]
+//user inputs "fart" 
+// let inputWord = inputWord.split("") <-- ["f", "a", "r", "t"]
+//let newWord = inputWord.shift <-- ["a", "r", "t"]
+//newWord.concat() 
+
+// word 
+// splitString = ["w", "o", "r", "d"]
+// sliceString = ["o", "r", "d"]
 
 
 
